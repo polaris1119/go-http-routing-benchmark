51526c795021ac2a905dc487141f2b876d72b392
xuxinhua <polaris@studygolang.com>
2019-12-19 18:16:19 +0800

    delete

--- routers_test.go
+++ routers_test.go
@@ -12,39 +12,14 @@ var (
 		name string
 		load func(routes []route) http.Handler
 	}{
-		{"Ace", loadAce},
-		{"Aero", loadAero},
-		{"Bear", loadBear},
 		{"Beego", loadBeego},
-		{"Bone", loadBone},
 		{"Chi", loadChi},
-		{"CloudyKitRouter", loadCloudyKitRouter},
-		{"Denco", loadDenco},
 		{"Echo", loadEcho},
 		{"Gin", loadGin},
-		{"GocraftWeb", loadGocraftWeb},
-		{"Goji", loadGoji},
-		{"Gojiv2", loadGojiv2},
-		{"GoJsonRest", loadGoJsonRest},
-		{"GoRestful", loadGoRestful},
 		{"GorillaMux", loadGorillaMux},
-		{"GowwwRouter", loadGowwwRouter},
 		{"HttpRouter", loadHttpRouter},
-		{"HttpTreeMux", loadHttpTreeMux},
-		//{"Kocha", loadKocha},
-		{"LARS", loadLARS},
 		{"Macaron", loadMacaron},
-		{"Martini", loadMartini},
-		{"Pat", loadPat},
-		{"Possum", loadPossum},
-		{"R2router", loadR2router},
 		// {"Revel", loadRevel},
-		{"Rivet", loadRivet},
-		//{"Tango", loadTango},
-		{"TigerTonic", loadTigerTonic},
-		{"Traffic", loadTraffic},
-		{"Vulcan", loadVulcan},
-		// {"Zeus", loadZeus},
 	}
 
 	// all APIs
@@ -53,8 +28,6 @@ var (
 		routes []route
 	}{
 		{"GitHub", githubAPI},
-		{"GPlus", gplusAPI},
-		{"Parse", parseAPI},
 		{"Static", staticRoutes},
 	}
 )
